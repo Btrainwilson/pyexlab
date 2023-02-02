154894e9fc1b3a739e3351e598a1713bf06628b2
Blake Wilson <wilso692@purdue.edu>
2023-02-01 23:26:26 -0500

    Update experiment.py
    Automatically generates datetime folder

--- pyexlab/experiments/experiment.py
+++ pyexlab/experiments/experiment.py
@@ -1,4 +1,4 @@
-from ..fileio import makeDirectory, save_pickle
+from ..fileio import makeDirectory, save_pickle, datetime_now_str
 
 class Experiment():
     """
@@ -26,13 +26,17 @@ class Experiment():
         Runs 100 epochs for each test subject and records the output to the save_folder
     """
 
-    def __init__(self, save_folder, test_subjects, print_report = True):
+    def __init__(self, save_folder, test_subjects, id="Experiment", print_report = True, pkl_copy = True):
 
-        self.save_folder = save_folder 
+        folder_name = id + datetime_now_str()
+        self.save_folder = makeDirectory(save_folder, folder_name)
+
         self.test_subjects = test_subjects
         self.print_report = print_report
+        self.pkl_copy = pkl_copy
 
         self.experiment_info = {}
+        self.analysis_info = {}
 
     def record(self):
 
@@ -40,7 +44,7 @@ class Experiment():
 
             #Pass up any information that needs to be recorded.
             self.experiment_info[test_subject.id(test_idx)] = test_subject.record(self.save_folder, test_idx)
-            
+        
 
     def run(self, epochs=100):
 
@@ -48,17 +52,47 @@ class Experiment():
 
             if self.print_report : print("Experiment Epoch %d" %(epoch))
 
-            for test_subject in self.test_subjects:
-                output = test_subject.measure(epoch = epoch)
+            for test_idx, test_subject in enumerate(self.test_subjects):
+
+                try:
+                    output = test_subject.measure(epoch = epoch)
+                except:
+                    print("Exception occurred during measurement of %s" %(test_subject.id(test_idx)))
+                    self.record()
+                    raise
 
                 if not output is None and self.print_report:
                     print(output)
 
-            self.record()
-        
+            
         #Final Measurements
-        for test_subject in self.test_subjects:
-            test_subject.final_measure()
+        for test_idx, test_subject in enumerate(self.test_subjects):
+            try:
+                test_subject.final_measure(epoch = epoch)
+            except:
+                print("Exception occurred during final measurement of %s" %(test_subject.id(test_idx)))
+                self.record()
+                raise
 
         #One last record
         self.record()
+        
+        if self.pkl_copy:
+            save_pickle(self.save_folder, "exp", self)
+
+    def analysis(self):
+
+        for test_idx, test_subject in enumerate(self.test_subjects):
+
+            #Pass up any information that needs to be recorded.
+            self.analysis_info[test_subject.id(test_idx)] = test_subject.analysis()
+        
+        return self.analysis_info
+
+    def graph(self):
+        for test_idx, test_subject in enumerate(self.test_subjects):
+
+            #Pass up any information that needs to be recorded.
+            self.analysis_info[test_subject.id(test_idx)] = test_subject.graph()
+        
+        return self.analysis_info
